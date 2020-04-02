54bdd9dd64f83a46e5f58d41adb0fea1f565a9b6
Shubham Shukla <shk.shubham@gmail.com>
2020-04-03 02:25:06 +0530

    changed permission of looking all

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -7,7 +7,7 @@ const router = Routes.Router();
 router.post('/auth', Auth.GuestAccess, UserController.auth);
 router.get('/logout', Auth.UserAccess, UserController.logout);
 router.get('/profile', Auth.UserAccess, UserController.profile);
-router.get('/looking/all', Auth.UserAccess, UserController.allLookingUsers);
+router.get('/looking/all', Auth.AllAccess, UserController.allLookingUsers);
 router.get('/looking/:type', Auth.UserAccess, UserController.lookingForGame);
 
 export default router;
