5c60bc9108908240455a3813bccd9cf10fcc4f7b
Shubham Shukla <shk.shubham@gmail.com>
2020-04-11 05:07:51 +0530

    added redis details

--- src/configs/index.js
+++ src/configs/index.js
@@ -7,6 +7,11 @@ class Config {
     static SOCKET_PORT = process.env.SOCKET_PORT;
     static DB_URL = process.env.DB_URL;
     static SECRET_KEY = process.env.SECRET_KEY || 'secret';
+    static REDIS = {
+      PASSWORD: process.env.REDIS_PASSWORD,
+      HOST: process.env.REDIS_HOST,
+      PORT: process.env.REDIS_PORT,
+    }
 }
 
 export default Config;
--- src/utils/queue.js
+++ src/utils/queue.js
@@ -1,9 +1,10 @@
 import Bull from 'bull';
 import Socket from '../services/socket.service';
 import Logger from './logger';
+import Config from '../configs';
 class Queue {
     static drawingQueue = new Bull('DRAWING')
-
+    static Redis = {redis: {port: Config.REDIS.PORT, host: Config.REDIS.HOST, password: Config.REDIS.PASSWORD}}
     static processDrawingQueue({data}, done) {
       Socket.io.emit('SERVER_DRAWING_TOUCH', data);
       Logger.log('log', data);
