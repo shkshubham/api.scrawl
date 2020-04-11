ed86fa3284fc8de98d439f4e35b0752433e92ef5
Shubham Shukla <shk.shubham@gmail.com>
2020-04-12 04:07:37 +0530

    added word check on backend

--- src/services/drawing.service.js
+++ src/services/drawing.service.js
@@ -1,7 +1,18 @@
 import Socket from './socket.service';
+const word = 'CYCLE';
 
 class Drawing {
   static processComments(roomCode, data) {
+    const {message} = data;
+    if (message.length) {
+      if (message.includes(word)) {
+        data.message = 'Guessed the word';
+      } else {
+        data.message = message[0];
+      }
+    } else {
+      data.message = '';
+    }
     Socket.emit(roomCode, data);
   }
 
