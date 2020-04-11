b14fc2baa51e1803cfe043d63ef36b3d36091bd0
Shubham Shukla <shk.shubham@gmail.com>
2020-04-12 05:23:11 +0530

    added guessed word feature

--- src/services/drawing.service.js
+++ src/services/drawing.service.js
@@ -2,10 +2,26 @@ import Socket from './socket.service';
 const word = 'cycle';
 
 class Drawing {
+  static types = {
+    LOBBY_CHAT: 'LOBBY_CHAT',
+    LOBBY_PLAYER_GUESSED_WORD: 'LOBBY_PLAYER_GUESSED_WORD',
+  }
   static processComments(roomCode, data) {
+    Drawing.checkAndSetWord(message);
+    Socket.emit(roomCode, {
+      type: Drawing.types.LOBBY_CHAT,
+      data,
+    });
+  }
+
+  static checkAndSetWord(data) {
     const {message} = data;
     if (message.length) {
       if (message.includes(word)) {
+        Socket.emit(roomCode, {
+          type: Drawing.types.LOBBY_PLAYER_GUESSED_WORD,
+          data: data.user.userId,
+        });
         data.message = 'Guessed the word';
       } else {
         data.message = message[0];
@@ -13,7 +29,6 @@ class Drawing {
     } else {
       data.message = '';
     }
-    Socket.emit(roomCode, data);
   }
 
   static processWords() {
@@ -22,3 +37,20 @@ class Drawing {
 }
 
 export default Drawing;
+
+
+// 42["lobbyReveal",{"reason":"TP","word":"lake","scores":[527,1140,537,2250,546,890,551,940,555,1830,565,650,567,0,568,0]}]
+// 42["chat",{"id":527,"message":"lake"}]
+// 42["lobbyChooseWord",{"id":555}]
+// 42["lobbyPlayerDrawing",555]
+// 42["lobbyCurrentWord","_____"]
+// 42["lobbyPlayerDrawing",551]
+
+// 42["lobbyReveal",{"reason":"TP","word":"short","scores":[527,1140,537,2700,546,890,551,940,555,2000,565,885,567,0,568,0]}]
+// 42["lobbyChooseWord",{"id":551}]
+// 42["lobbyPlayerDrawing",551]
+// 42["lobbyCurrentWord","_______"]
+// 42["canvasClear"]
+// 42["lobbyCurrentWord","___f___ _____"]
+// 42["lobbyPlayerGuessedWord",565]
+// 42["lobbyTime",31]
