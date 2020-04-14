4665b355b7b3b3e626b2e3e13640dddc773686fb
Shubham Shukla <shk.shubham@gmail.com>
2020-04-15 01:48:14 +0530

    changed room urls to lobby

--- src/routes/lobby.routes.js
+++ src/routes/lobby.routes.js
@@ -1,8 +1,18 @@
 import Routes from './app';
 import LobbyController from '../controllers/lobby.controller';
+import RoomController from '../controllers/room.controller';
 
 const router = Routes.Router();
-
+router.get('/create', Auth.UserAccess, RoomController.createRoom);
+router.get('/join/:roomCode', Auth.UserAccess, RoomController.roomJoin);
+router.get('/leave/:roomCode', Auth.UserAccess, RoomController.leaveRoom);
+router.get('/search/:roomCode/', Auth.UserAccess, RoomController.roomDetail);
+router.post('/kick/:roomCode/', Auth.UserAccess, RoomController.kickPlay);
+router.post('/edit/:roomCode/', Auth.UserAccess, RoomController.editRoom);
+router.get('/public', Auth.AllAccess, RoomController.listAllPublic);
 router.get('/init', LobbyController.getLobbyDetails);
 
 export default router;
+
+
+// TODO: To fix if the user leave the looby
